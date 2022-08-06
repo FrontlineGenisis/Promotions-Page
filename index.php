af650de44d4f0fa1dcdcde679b8f9364a187fdd7
Marshall Woods <80414685+FrontlineGenisis@users.noreply.github.com>
2022-08-06 12:02:32 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+  <head>
+    <title>Promotions | Frontline Genisis</title>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@thefrontlinegenisis">
+    <meta name="twitter:title" content="Promotions | Frontline Genisis">
+    <meta name="twitter:description" content="Frontline Genisis promotions for collabs and partnerships.">
+    <meta name="twitter:image" content="https://thefrontlinegenisis.ml/assets/img/favicon/favicon.png">
+    <link rel="stylesheet" type="text/css" href="./assets/css/main.css" media="screen" /><!--
+    <link rel="stylesheet" type="text/css" href="./assets/fonts/fonts.css" />-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/js/all.min.js" crossorigin="anonymous" referrerpolicy="no-referrer">
+    </script>
+    <link rel="shortcut icon" href="https://thefrontlinegenisis.ml/assets/img/favicon/favicon.ico" />
+    <link rel="apple-touch-icon-precomposed" href="https://thefrontlinegenisis/assets/img/favicon/favicon.png" />
+  </head>
+  <body>
+  <div class="loader">
+  <div class="ring">
+  </div>
+    </div>
+  </body>
+      <script>
+        const loader = document.querySelector(".loader");
+          window.onload = function(){
+            setTimeout(function(){
+              loader.style.opacity = "0";
+              setTimeout(function(){
+                loader.style.display = "none";
+              }
+                         , 500);
+            }
+                       ,1500);
+          }
+    </script>
+</html>
